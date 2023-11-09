f26b8a180a484bb59962e5e84fc41606d2c7f5a0
livaieleja <livaieleja@gmail.com>
2023-11-09 20:50:47 +0200

    Otrais Commit

--- Day2_if/Day2_if/src/main/java/org/example/Main.java
+++ Day2_if/Day2_if/src/main/java/org/example/Main.java
@@ -2,14 +2,42 @@ package org.example;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        task1();
+        sample3();
 
     }
+
+    public static void sample3(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ievadiet skaitli!");
+        int a = sc.nextInt();
+
+        // Izvadīt "jā", ja skaitlis ir pozitīvs un "nē", ja skaitlis ir negatīvs un izvadīt kļūdas paziņojumu, ja ir 0!
+        // ideja: skaitlis var būt 0 >, <, >=, <=, ==, !=
+        if(a > 0) {
+            System.out.println("Jā");
+        }else if(a < 0) {
+            System.out.println("Nē");
+        }else{
+            System.out.println("Kļūda");
+        }
+
+        //String if
+        //ir vai nav vienāds
+        String b = sc.nextLine();
+        if(b.equals("bbb")){
+            System.out.println("ir vienāds");
+        }
+
+        }
+
+
+
     public static void task1(){
         Scanner sc = new Scanner(System.in);
 
