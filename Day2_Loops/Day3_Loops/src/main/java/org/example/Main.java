251da7bacabe3d952d09472a878dc65e76f2e4ef
livaieleja <livaieleja@gmail.com>
2023-11-14 19:57:01 +0200

    Kommits ceturtais

--- Day2_Loops/Day3_Loops/src/main/java/org/example/Main.java
+++ Day2_Loops/Day3_Loops/src/main/java/org/example/Main.java
@@ -4,8 +4,39 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        task1 ();
+        task2 ();
     }
+
+    public static void task2() {
+        Scanner sc = new Scanner(System.in);
+
+        int len = 0;
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Ievadiet simbolu virkni");
+            String input = sc.nextLine();
+            len = len + input.length();
+        }
+        System.out.println("Simbolu virknes kopējais garums ir " + len);
+
+    }
+
+
+    public static void sample2() {
+        Scanner sc = new Scanner(System.in);
+
+
+        int b = 0; //iedod skaitlim vērtību, tas ir obligāti jāizdara un pēc tam ciklā mainām vērtību.
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Ievadiet skaitli");
+            int a = sc.nextInt();
+            b = b + a;
+            System.out.println(a);
+            System.out.println("----------------");
+        }
+        System.out.println("Summa ir " + b);
+
+    }
+
 
     public static void task1() {
         Scanner sc = new Scanner(System.in);
