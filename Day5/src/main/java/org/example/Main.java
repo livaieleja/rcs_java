36c22853bfd01fcb8547833066029085b73873b2
livaieleja <livaieleja@gmail.com>
2023-11-21 18:37:44 +0200

    Sestais kommits

---
+++ Day5/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        task1("ss", 3);
+
+
+    }
+    public static void task1(String a, int b) {
+        String symbol = "";
+        for (int i = 0; i < b; i++) {
+            symbol = symbol + " ss";
+            System.out.println(symbol);
+        }
+    }
+
+}
