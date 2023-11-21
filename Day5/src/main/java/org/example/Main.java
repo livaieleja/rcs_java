f6c674eaafc5513728ff5ad735ee830d2ecc60a5
livaieleja <livaieleja@gmail.com>
2023-11-21 19:40:37 +0200

    Sestais komits

--- Day5/src/main/java/org/example/Main.java
+++ Day5/src/main/java/org/example/Main.java
@@ -1,17 +1,57 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        task1("ss ", 3);
+        int result = task2(2,7);
+        System.out.println(result);
+    //    System.out.println(combineStrings("e"));
+    //    int a = sum();
+    //    System.out.println(a);
+    //    task1("ss", 3);
+    //    task1 ("JJ", 5);
+    }
 
+
+    public static int task2 (int a, int b){
+        if (a > b) {
+            return a;
+        }else{
+            return b;
+        }
+    }
+
+    public static String combineStrings(String param){
+        // Uztaisīt metodi, kura paņem vienu string tipa vērtību kā parametru
+        // šo vērtību saglabāt citā stringā 10x
+        // Vērtību padod ar return
+        // piemērs -> parametrs = "a" -> ar return "aaaaaaaaaa"
+        String result = "";
+        for(int i = 0; i < 10; i++){
+            result = result + param;
+        }
+
+        return result;
+
+    }
+
+    public static int sum(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ievadiet skaitli!");
+        int sk1 = sc.nextInt();
+
+        System.out.println("Ievadiet skaitli!");
+        int sk2 = sc.nextInt();
+
+        return sk1 + sk2;
 
     }
     public static void task1(String a, int b) {
         String symbol = "";
         for (int i = 0; i < b; i++) {
-            symbol = symbol + a;
+            symbol = symbol + " " + a;
             System.out.println(symbol);
         }
     }
-
 }
