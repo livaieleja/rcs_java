c51c389ff56f2b25cf1d0a200017e352c3070fe3
livaieleja <livaieleja@gmail.com>
2023-11-21 18:45:40 +0200

    Sestais kommits

--- Day5/src/main/java/org/example/Main.java
+++ Day5/src/main/java/org/example/Main.java
@@ -2,14 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        task1("ss", 3);
+        task1("ss ", 3);
 
 
     }
     public static void task1(String a, int b) {
         String symbol = "";
         for (int i = 0; i < b; i++) {
-            symbol = symbol + " ss";
+            symbol = symbol + a;
             System.out.println(symbol);
         }
     }
