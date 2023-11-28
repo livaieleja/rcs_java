f6be159028ebc5ce82ee54f3e6f38707042fad92
livaieleja <livaieleja@gmail.com>
2023-11-28 19:20:15 +0200

    Septītais komits

--- Day7_lists/src/main/java/org/example/Main.java
+++ Day7_lists/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args){
-        int [] a = {2,5790,6,7,12,56};
+        int [] a = {2,3,6,7,12,56};
         System.out.println(Tasks.task1(a));
     }
 }
--- Day7_lists/src/main/java/org/example/Tasks.java
+++ Day7_lists/src/main/java/org/example/Tasks.java
@@ -3,7 +3,7 @@ package org.example;
 public class Tasks {
 
     public static double task1(int[] number) {
-        double biggest = 0;
+        double biggest = Integer.MIN_VALUE;
 
         for (int i = 0; i < number.length; i++) {
             if (number[i] > biggest) {
