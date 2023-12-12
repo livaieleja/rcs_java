d36c14273ba5ef312193be8bda7b0a6cb02acf27
livaieleja <livaieleja@gmail.com>
2023-12-12 18:14:41 +0200

    Trīsmadsmitais kommits

--- SQL/day9.sql
+++ SQL/day9.sql
@@ -1,18 +1,40 @@
+CREATE TABLE Owners(
+    id INTEGER PRIMARY KEY,
+    name TEXT (30)
+);
+
 CREATE TABLE Pets(
     id INTEGER PRIMARY KEY,
     name TEXT (30) NOT NULL,
-    type TEXT (20)
+    type TEXT (20),
+    ownerId INTEGER,
+    FOREIGN KEY (ownerId) REFERENCES Owners (id)
 );
+
+DROP TABLE Pets;
+
+INSERT INTO Owners(id, name)
+VALUES
+(1, "Jānis"),
+(2, "Anna");
+
+SELECT * FROM Owners;
 
 INSERT INTO Pets(id, name, type)
 VALUES (1, "Muris", "Kaķis");
 
-INSERT INTO Pets(id, name, type)
-VALUES (2, "Rada", "Suns");
+INSERT INTO Pets(id, name, type, ownerId)
+VALUES (2, "Rada", "Suns", 1);
 
-INSERT INTO Pets(id, name, type)
-VALUES (3, "Poga", "Suns");
+INSERT INTO Pets(id, name, type, ownerId)
+VALUES (3, "Poga", "Suns", 2);
     
 SELECT * FROM Pets;
 
-SELECT name, type FROM Pets;
+SELECT name, type
+FROM Pets
+WHERE type = "Kaķis";
+
+SELECT Pets.name, Pets.type, Owners.name 
+FROM Pets, Owners
+WHERE Pets.ownerId = Owners.id;
