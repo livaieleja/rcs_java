62122ea79d8de966b747b338d0d5e1f834d76f3d
livaieleja <livaieleja@gmail.com>
2024-01-16 20:34:36 +0200

    Astoņpadsmitais kommits

--- day7-angular/src/app/app.component.ts
+++ day7-angular/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
 import { FormsModule } from '@angular/forms';
+
+import { HelloComponent } from './hello/hello.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule],
+  imports: [CommonModule, RouterOutlet, FormsModule,HelloComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
