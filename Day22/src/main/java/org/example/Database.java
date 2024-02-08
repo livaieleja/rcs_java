79f24922cf7ff2fb16127c4fb926c3203d7749ab
livaieleja <livaieleja@gmail.com>
2024-02-08 21:13:47 +0200

    Divdesmitotrais kommits - 3

---
+++ Day22/src/main/java/org/example/Database.java
@@ -0,0 +1,36 @@
+package org.example;
+import com.google.gson.Gson;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+
+        public static List<Employee> getEmployees() {
+            String url = "jdbc:sqlite:/Users/livaieleja/Desktop/Kursi - RCS/JAVA/rcs_java/Day22/day22.db";
+            List<Employee> employees = new ArrayList<>();
+
+            try {
+                Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT firstName, lastName, yearsWorked FROM employee");
+
+                while (rs.next()) {
+                    Employee employee = new Employee(
+                            rs.getString("firstName"),
+                            rs.getString("lastName"),
+                            rs.getInt("yearsWorked"));
+                    employees.add(employee);
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+
+            return employees;
+        }
+    public static String convertListToJson(List<Employee> employees) {
+        Gson gson = new Gson();
+        return gson.toJson(employees);
+    }
+}
--- Day22/src/main/java/org/example/Main.java
+++ Day22/src/main/java/org/example/Main.java
@@ -1,22 +1,29 @@
 package org.example;
-
-import java.util.Scanner;
+import com.google.gson.Gson;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Ievadiet darbinieka vārdu:");
-        String firstName = scanner.nextLine();
-
-        System.out.println("Ievadiet darbinieka uzvārdu:");
-        String lastName = scanner.nextLine();
-
-        System.out.println("Iedvadiet darbinieka darba stāžu:");
-        int yearsWorked = scanner.nextInt();;
-
-        Employee employee = new Employee(firstName, lastName, yearsWorked);
-
-        employee.displayInfo();
+        List<Employee> employees = Database.getEmployees();
+        String json = Database.convertListToJson(employees);
+        System.out.println(json);
     }
 }
+
+    //Scanner scanner = new Scanner(System.in);
+
+        //System.out.println("Ievadiet darbinieka vārdu:");
+        //String firstName = scanner.nextLine();
+
+        //System.out.println("Ievadiet darbinieka uzvārdu:");
+        //String lastName = scanner.nextLine();
+
+        //System.out.println("Iedvadiet darbinieka darba stāžu:");
+        // int yearsWorked = scanner.nextInt();;
+
+        //Employee employee = new Employee(firstName, lastName, yearsWorked);
+
+        //employee.displayInfo();
+
